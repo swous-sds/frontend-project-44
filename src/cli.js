67823964b9fe51af1c21df0610485cb43a20cd13
swous-sds <rkhnShtn@hotmail.com>
2022-12-06 19:02:46 +0500

    completing 3 steps

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const greeting = () => {
+  const name = readlineSync.question('Your answer: ');
+  console.log(`Hello, ${name}`);
+};
+export default greeting;
