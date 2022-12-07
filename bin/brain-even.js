3edb8d97a162267fb6de696f235b193ed9e4539f
swous-sds <rkhnShtn@hotmail.com>
2022-12-07 22:39:27 +0500

    Update step 5

---
+++ bin/brain-even.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+//import greeting from '../src/core.js';
+import readlineSync from 'readline-sync';
+const greeting = () => {
+	console.log('Welcome to the Brain Games!');
+	const name = readlineSync.question('May I have your name?: ');
+	console.log(`Hello, ${name}!`); // это добавили 
+
+   console.log('Answer "yes" if the number is even, otherwise answer "no".');
+   const getRandomNember = (min = 0, max = 50) => {
+      const random = Math.floor(min - 0.5 + Math.random() * (max - min + 1));
+      return Math.round(random); // ДЕЛАЕМ РАНДОМ ЧИСЕЛ ЧТОБЫ НАМ ОНИ ВЫВЕЛИСЬ ХОТЬ ПОН
+   }
+  
+   for (let i=0; i < 3; i++) { // ХЗ БЛИН ЭТОТ ЦИКЛ МИНУС МИНИМУМ Я НЕ УВЕРЕНА
+   const theNumber = getRandomNember(); 
+   console.log(`Question: ${theNumber}`);
+   const answersUser = readlineSync.question('Your answer: ');
+   const theEvenNumber = (theNumber % 2 === 0) && (answersUser === 'yes');
+   const theOddNumber = (theEvenNumber % 2 !== 0) && (answersUser === 'no'); // ОТВЕТЫ ЧЕЛИКА ДАЛЬШЕ БУДЕТ ПРОВЕРКА
+   if (theEvenNumber === true || theOddNumber === true) {
+      console.log('Correct!')
+   } else {
+      const correctAnswers = (answersUser === 'yes' ? 'no' : 'yes'); // ЧТОБЫ ВЫВОДИЛСЯ ОТВЕТ ДА ИЛИ НЕТ ДЛЯ ЛОХА КОТОРЫЙ НЕ РЕШИЛ
+      console.log(`'${correctAnswers}' is wrong answer ;(. Correct answer was '${correctAnswers}'.`)
+      console.log(`Let's try again, ${name}`)
+      return;
+   };
+   console.log(`Congratulations, ${name}!`);
+   };
+};
+greeting();
+export default greeting;
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import greeting from '../src/cli.js';
-
+//import greeting from '../src/cli.js';
+import greeting from './brain-even';
 console.log('Welcome to the Brain Games!')
 
 greeting();
